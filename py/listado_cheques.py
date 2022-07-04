f242f85958aca9119d52d7575fc1533829861ffb
maleaznarez <105256723+maleaznarez@users.noreply.github.com>
2022-07-04 02:12:07 -0300

    agregue filtro por dni

--- py/listado_cheques.py
+++ py/listado_cheques.py
@@ -4,7 +4,11 @@ with open('csv/listado.csv') as f:
     reader= csv.reader(f, delimiter=',')
     header = next(reader)
     for row in reader:
-        print("NroCheque: {0},CodigoBanco: {1},CodigoScurusal: {2},NumeroCuentaOrigen: {3},NumeroCuentaDestino: {4},Valor: {5},FechaOrigen: {6},FechaPago: {7},DNI: {8},Tipo: {9},Estado:{10}".format(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10]))
-
+        i = 0
+        dniCVS = []
+        dniCVS.append(int(row[8]))
+        if dniCVS[i] == dni:
+            print("NroCheque: {0},CodigoBanco: {1},CodigoScurusal: {2},NumeroCuentaOrigen: {3},NumeroCuentaDestino: {4},Valor: {5},FechaOrigen: {6},FechaPago: {7},DNI: {8},Tipo: {9},Estado:{10}".format(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10]))
+        i += 1
 
 #cheque.close
