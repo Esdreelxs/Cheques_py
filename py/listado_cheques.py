84f07aa887b73f58d3206eab5575f1c38a7b8aac
eugordinho <martinsanchezr@outlook.com>
2022-07-04 00:32:07 -0300

    Estructura

---
+++ py/listado_cheques.py
@@ -0,0 +1,16 @@
+import csv
+
+#cheque  = open("", "w")
+
+dni = int(input("Ingrese el DNI: "))
+
+salida = input("\nComo desea visualizar los datos?(PANTALLA o CSV): ")
+
+tipo = input("Tipo de cheque?")
+
+
+
+
+
+
+#cheque.close
