15a4af1b2462ec5e62b8dbbfac769eb14b28ed7b
eugordinho <martinsanchezr@outlook.com>
2022-07-04 00:52:44 -0300

    Estructura

--- py/listado_cheques.py
+++ py/listado_cheques.py
@@ -4,12 +4,15 @@ cheque  = open("csv/listado.csv", "r")
 
 dni = int(input("Ingrese el DNI: "))
 
-salida = input("\nComo desea visualizar los datos?(PANTALLA o CSV): ")
+salida = input("\nEscriba como desea visualizar los datos (PANTALLA o CSV): ")
 
-tipo = input("Tipo de cheque?")
+tipo = input("\nTipo de cheque (EMITIDO o DEPOSITADO): ")
 
+estado = input("\nEstado del cheque (PENDIENTE, APROBADO o RECHAZADO): ")
 
+fecha = input("\nIngrese un rango de fechas (dd-mm-aaaa - dd-mm-aaaa): ")
 
+if dni == 
 
 
 
