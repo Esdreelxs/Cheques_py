2da618f1606ffd45b58b2a8b37348201602d686f
maleaznarez <105256723+maleaznarez@users.noreply.github.com>
2022-07-08 13:39:44 -0300

    Update listado_cheques.py

--- py/listado_cheques.py
+++ py/listado_cheques.py
@@ -18,25 +18,18 @@ tipo = input("\nTipo de cheque (EMITIDO o DEPOSITADO):\n")
 estado = input("\nEstado del cheque (PENDIENTE, APROBADO o RECHAZADO)(opcional):\n")
 rangFecha = input("\nIngrese un rango de fechas (dd-mm-aaaa:dd-mm-aaaa)(opcional):\n")
 
-if rangFecha != '':
-    date1 = rangFecha[0:10]
-    date1 = datetime.datetime.strptime(date1,r"%d-%m-%Y")
-    date1 = datetime.datetime.timestamp(date1)
-    date2 = rangFecha[11:21]
-    date2 = datetime.datetime.strptime(date2,r"%d-%m-%Y")
-    date2 = datetime.datetime.timestamp(date2)
+i = 0
+dniCSV = []
+tipoCSV = []
+estadoCSV = []
+fechaOrCSV = []
+fechaPgCSV = []          
 
 with open(archivo) as f:
     reader= csv.reader(f, delimiter=',')
     header = next(reader)
     if salida == "PANTALLA":
         for row in reader:
-            i = 0
-            dniCSV = []
-            tipoCSV = []
-            estadoCSV = []
-            fechaOrCSV = []
-            fechaPgCSV = []
             dniCSV.append(row[8])
             tipoCSV.append(row[9])
             estadoCSV.append(row[10])
@@ -45,15 +38,23 @@ with open(archivo) as f:
             if dniCSV[i] == dni:
                 if tipoCSV[i] == tipo:
                     if estadoCSV[i] == estado or estado == '':
-                        if (fechaOrCSV[i] >= date1 and fechaPgCSV[i] <= date2) or rangFecha == '':
-                            print("NroCheque: {0},CodigoBanco: {1},CodigoScurusal: {2},NumeroCuentaOrigen: {3},NumeroCuentaDestino: {4},Valor: {5},FechaOrigen: {6},FechaPago: {7},DNI: {8},Tipo: {9},Estado:{10}".format(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10]))
-
+                        if rangFecha == '':
+                        print("NroCheque: {0},CodigoBanco: {1},CodigoScurusal: {2},NumeroCuentaOrigen: {3},NumeroCuentaDestino: {4},Valor: {5},FechaOrigen: {6},FechaPago: {7},DNI: {8},Tipo: {9},Estado:{10}".format(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10]))
+                        else: 
+                            date1 = rangFecha[0:10]
+                            date1 = datetime.datetime.strptime(date1,r"%d-%m-%Y")
+                            date1 = datetime.datetime.timestamp(date1)
+                            date2 = rangFecha[11:21]
+                            date2 = datetime.datetime.strptime(date2,r"%d-%m-%Y")
+                            date2 = datetime.datetime.timestamp(date2)
+                            if fechaOrCSV[i] >= date1 and fechaPgCSV[i] <= date2:
+                                print("NroCheque: {0},CodigoBanco: {1},CodigoScurusal: {2},NumeroCuentaOrigen: {3},NumeroCuentaDestino: {4},Valor: {5},FechaOrigen: {6},FechaPago: {7},DNI: {8},Tipo: {9},Estado:{10}".format(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10]))
+            i += 1  
     elif salida == "CSV":
         now = datetime.datetime.now()
         date_time = now.strftime(r"%d-%m-%Y_%H.%M.%S")      
         with open(('csv/' + dni + '_' + date_time + '.csv'), "w") as f:
                     f.write(sys.argv[2])
-
     else:
         print("Argumento invalido")
                 
